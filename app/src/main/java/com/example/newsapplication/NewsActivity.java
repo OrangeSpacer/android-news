655bc89844e519d93bf42a0fd6eee9e84a283898
OrangeSpacer <maxaev.art@yandex.ru>
2023-05-05 13:29:27 +0300

    feat: add script for load news

--- app/src/main/java/com/example/newsapplication/NewsActivity.java
+++ app/src/main/java/com/example/newsapplication/NewsActivity.java
@@ -1,105 +1,164 @@
 package com.example.newsapplication;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Xml;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.widget.TextView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.tabs.TabLayout;
+
+import org.jetbrains.annotations.NotNull;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.List;
 
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-public class NewsActivity extends AppCompatActivity {
-
-    OkHttpClient client = new OkHttpClient();
-    private String URL = "https://news.rambler.ru/rss/politics/";
-    private TextView mTextView;
-    @Override
+public class NewsActivity extends AppCompatActivity{
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
 
-        mTextView = findViewById(R.id.linear_layout);
-        WebView webView = findViewById(R.id.webView);
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager = findViewById(R.id.view_pager);
 
+        List<String> tabTitles = new ArrayList<>();
+        tabTitles.add("politics");
+        tabTitles.add("technology");
+        tabTitles.add("technology");
 
-        new GetNewsTask().execute("https://news.rambler.ru/rss/politics/");
+        for (int i = 0; i < tabTitles.size(); i++) {
+            tabLayout.addTab(tabLayout.newTab().setText(tabTitles.get(i)));
+        }
+
+            MyPagerAdapter adapter = new MyPagerAdapter(getSupportFragmentManager(), tabTitles);
+            viewPager.setAdapter(adapter);
+            tabLayout.setupWithViewPager(viewPager);
     }
 
-    
-    private class GetNewsTask extends AsyncTask<String, Void, ArrayList<String>> {
+    private static class MyPagerAdapter extends FragmentPagerAdapter {
+        private List<String> tabTitles;
+
+        public MyPagerAdapter(@NonNull FragmentManager fm, List<String> tabTitles) {
+            super(fm);
+            this.tabTitles = tabTitles;
+        }
+
+        @NonNull
         @Override
-        protected ArrayList<String> doInBackground(String... urls) {
+        public Fragment getItem(int position) {
+            return TabFragment.newInstance(position);
+        }
+
+        @Override
+        public int getCount() {
+            return tabTitles.size();
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return tabTitles.get(position);
+        }
+    }
+
+    public static class TabFragment extends Fragment {
+        private ListView listView;
+        private List<String> titles = new ArrayList<>();
+
+
+        public static TabFragment newInstance(int position) {
+            TabFragment fragment = new TabFragment();
+            Bundle args = new Bundle();
+            args.putInt("position", position);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.fragment_tab, container, false);
+            listView = view.findViewById(R.id.listView);
+            return view;
+        }
+
+        @Override
+        public void onViewCreated(@NotNull View view, Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+            int position = getArguments().getInt("position");
+            String reqQuery = "";
+            switch (position){
+                case 0:
+                    reqQuery = "politics";
+                    break;
+                case 1:
+                    reqQuery = "technology";
+                    break;
+            }
             OkHttpClient client = new OkHttpClient();
-
+            System.out.println("https://news.rambler.ru/rss/" +  reqQuery + "/");
             Request request = new Request.Builder()
-                    .url(urls[0])
+                    .url("https://news.rambler.ru/rss/" +  reqQuery + "/")
                     .build();
-
-            try {
-                Response response = client.newCall(request).execute();
-                InputStream inputStream = response.body().byteStream();
-                return parseXML(inputStream);
-            } catch (IOException e) {
-                e.printStackTrace();
-                return null;
-            } catch (XmlPullParserException e) {
-                e.printStackTrace();
-                return null;
-            }
-        }
-
-        @Override
-        protected void onPostExecute(ArrayList<String> result) {
-            if (result != null) {
-                for (String s : result) {
-                    mTextView.append(s + "\n\n");
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                    e.printStackTrace();
                 }
-            } else {
-                mTextView.setText("Ошибка загрузки данных");
-            }
-        }
-    }
 
-    private ArrayList<String> parseXML(InputStream inputStream) throws XmlPullParserException, IOException {
-        ArrayList<String> titles = new ArrayList<>();
+                @Override
+                public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                    if (!response.isSuccessful()) {
+                        throw new IOException("Unexpected code " + response);
+                    }
+                    String xml = response.body().string();
+                    try{
+                        XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+                        factory.setNamespaceAware(true);
+                        XmlPullParser xpp = factory.newPullParser();
+                        xpp.setInput(new StringReader(xml));
 
-        XmlPullParser parser = Xml.newPullParser();
-        parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
-        parser.setInput(inputStream, null);
+                        int eventType = xpp.getEventType();
+                        while(eventType != XmlPullParser.END_DOCUMENT) {
+                            if(eventType == XmlPullParser.START_TAG && xpp.getName().equals("title")){
+                                eventType = xpp.next();
+                                String title = xpp.getText();
+                                titles.add(title);
+                            }
+                            eventType = xpp.next();
+                        }
+                        getActivity().runOnUiThread(() -> {
+                            ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, titles);
+                            listView.setAdapter(adapter);
+                        });
 
-        int eventType = parser.getEventType();
-        String title = null;
-        while (eventType != XmlPullParser.END_DOCUMENT) {
-            String name = parser.getName();
-
-            if (eventType == XmlPullParser.START_TAG) {
-                if (name.equalsIgnoreCase("title")) {
-                    title = parser.nextText();
+                    } catch (XmlPullParserException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
-            } else if (eventType == XmlPullParser.END_TAG) {
-                if (name.equalsIgnoreCase("item")) {
-                    titles.add(title);
-                    title = null;
-                }
-            }
-
-            eventType = parser.next();
+            });
         }
-
-        return titles;
     }
 }
