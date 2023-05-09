9335b5f65a4197ed03e18a80de2d48868d424656
OrangeSpacer <maxaev.art@yandex.ru>
2023-05-09 15:31:55 +0300

    feat: add styles for login page

--- app/src/main/java/com/example/newsapplication/MainActivity.java
+++ app/src/main/java/com/example/newsapplication/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,78 +25,63 @@ import okhttp3.Response;
 
 
 public class MainActivity extends AppCompatActivity {
-    public interface LoginCallback {
-        void onSuccess(JSONObject response) throws JSONException;
-        void onFailure(Exception e);
-    }
-
-    public static JSONObject fetchToServer(String login, String password, LoginCallback callback) {
-        OkHttpClient client = new OkHttpClient();
-        String API_URL = "https://android-for-students.ru/coursework/login.php";
-
-        RequestBody formBody = new FormBody.Builder()
-                .add("lgn",login)
-                .add("pwd", password)
-                .add("g", "RIBO-01-21")
-                .build();
-        Request request = new Request.Builder()
-                .url(API_URL)
-                .post(formBody)
-                .build();
-
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                callback.onFailure(e);
-            }
-
-            @Override
-            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                String json = response.body().string();
-
-                try {
-                    JSONObject jsonObject = new JSONObject(json);
-                    callback.onSuccess(jsonObject);
-                } catch (JSONException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
-        return null;
-    }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView errorView = findViewById(R.id.error);
         EditText loginText = findViewById(R.id.loginText);
         EditText passwordText = findViewById(R.id.passwordText);
         Button loginBtn = findViewById(R.id.loginBtn);
 
-        loginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                JSONObject res = fetchToServer(loginText.getText().toString(), passwordText.getText().toString(), new LoginCallback() {
-                    @Override
-                    public void onSuccess(JSONObject response) throws JSONException {
-                        if(response.getInt("result_code") == -1) {
-                            System.out.println(response);
-                        } else {
-                            Intent actIntent = new Intent(getApplicationContext(), NewsActivity.class);
-                            startActivity(actIntent);
-                        }
-                    }
+        loginBtn.setOnClickListener(view -> sendReq(errorView,loginText,passwordText));
+    }
 
-                    @Override
-                    public void onFailure(Exception e) {
-                        System.out.println(e);
+    private void sendReq(TextView errorBlock,EditText login,EditText password) {
+        OkHttpClient client = new OkHttpClient();
+
+        String API_URL = "https://android-for-students.ru/coursework/login.php";
+
+        RequestBody formBody = new FormBody.Builder()
+                .add("lgn",login.getText().toString())
+                .add("pwd", password.getText().toString())
+                .add("g", "RIBO-01-21")
+                .build();
+        Request req = new Request.Builder()
+                .url(API_URL)
+                .post(formBody)
+                .build();
+
+        client.newCall(req).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                String res = response.body().string();
+
+                runOnUiThread(() -> {
+                    try{
+                        JSONObject jsonObject = new JSONObject(res);
+                        int resultCode = jsonObject.getInt("result_code");
+                        if(resultCode == 1){
+                            errorBlock.setVisibility(View.INVISIBLE);
+                            Intent intent = new Intent(MainActivity.this, NewsActivity.class);
+                            startActivity(intent);
+                        } else if (resultCode == -1){
+                            System.out.println(jsonObject);
+                            String error = jsonObject.getString("error");
+                            errorBlock.setVisibility(View.VISIBLE);
+                            errorBlock.setText(error);
+                        }
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
                     }
                 });
             }
         });
     }
 }
-
-//    Intent actIntent = new Intent(getApplicationContext(), NewsActivity.class);
-//    startActivity(actIntent);
