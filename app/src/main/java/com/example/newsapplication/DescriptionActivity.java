9e218a56824c9a92234fe019bd0af4b47a274fcf
OrangeSpacer <maxaev.art@yandex.ru>
2023-05-15 17:26:32 +0300

    feat: add share btn

--- app/src/main/java/com/example/newsapplication/DescriptionActivity.java
+++ app/src/main/java/com/example/newsapplication/DescriptionActivity.java
@@ -1,6 +1,9 @@
 package com.example.newsapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,6 +11,8 @@ import androidx.appcompat.app.AppCompatActivity;
 public class DescriptionActivity extends AppCompatActivity {
     private TextView textView;
     private TextView titleText;
+
+    private Button shareBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +23,20 @@ public class DescriptionActivity extends AppCompatActivity {
 
         textView = findViewById(R.id.descriptionTextView);
         titleText = findViewById(R.id.titleTextView);
+        shareBtn = findViewById(R.id.shareBtn);
         titleText.setText(title);
         textView.setText(description);
+
+        shareBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String textToShare = textView.getText().toString();
+
+                Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                shareIntent.setType("text/plain");
+                shareIntent.putExtra(Intent.EXTRA_TEXT, textToShare);
+                startActivity(Intent.createChooser(shareIntent, "Share via"));
+            }
+        });
     }
 }
